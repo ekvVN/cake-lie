5df8c3fa96122ddea3475048e4d1ef552bed9765
Ekventor <zzespoozz@gmail.com>
2017-06-10 02:17:15 +0300

    +Add NLog + Commmon.Logging +Fix change target bin dir -> ../Debug, ../Release

---
+++ LogConf.CakeLie/LogExample.cs
@@ -0,0 +1,17 @@
+﻿namespace LogConf.CakeLie
+{
+    using Common.Logging;
+
+    /// <summary>
+    /// LogExample - пример записи в лог
+    /// </summary>
+    public static class LogExample
+    {
+        private static readonly ILog _log = LogManager.GetLogger("LogExample");
+
+        public static void Start()
+        {
+            _log.Info("Hello NLog from Common.Logging");
+        }
+    }
+}
